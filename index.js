9e0a56cab601715f2b77eb08a9db0ccfbe49aa57
Victorien01 <victorienboin95@gmail.com>
2020-03-31 18:27:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const bot = new Discord.Client({DisableEveryone: true});
 const config = require('./config.js');
 
 
 bot.on('ready', async () => {
+  console.log("Le bot est lancé !");
   bot.user.setActivity("Je suis le nouveau !")
 });
+
+bot.on('message', async (msg) => {
+  if(msg.content.startsWith(config.prefix.length) && !msg.author.bot) {
+    cmdArray = msg.content.split()
+    cmd = cmdArray[0]
+    args = cmdarray.slice(1)
+
+    msg.channel.send(cmd)
+  }
+})
 
 bot.login(config.token);
