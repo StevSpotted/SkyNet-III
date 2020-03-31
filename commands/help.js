27199ff676e9f8dbc9cfa7d2b98b9dabd602271d
Victorien01 <victorienboin95@gmail.com>
2020-03-31 21:18:27 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
-const config = require('./config.js');
+const prefix = "*"
 module.exports.run = async (bot, msg, args) => {
 
   let hEmbed = new Discord.MessageEmbed()
   .setColor('5DF510')
   .setTitle(`Pannel d'aide`)
   .setDescription('Ici vous trouverez toutes les commandes disponibles !')
-  .addField(`${config.prefix}administration`, `Vous donneras l'accès au commandes administrateurs`)
-  .addField(`${config.prefix}modération`, `Vous donneras l'accès au commandes modérateurs`)
-  .addField(`${config.prefix}En dev`, 'En dev..')
+  .addField( prefix + `administration`, `Vous donneras l'accès au commandes administrateurs`)
+  .addField( prefix + `modération`, `Vous donneras l'accès au commandes modérateurs`)
+  .addField(`En dev`, 'En dev..')
   .setFooter('[Support serveur](https://discord.gg/t2tqR3b)', true )
   .setTimestamp();
   msg.channel.send(hEmbed);
