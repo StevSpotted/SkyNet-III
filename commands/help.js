f30716cf0686023da54ad00f65006f351bd9e158
Victorien01 <victorienboin95@gmail.com>
2020-03-31 17:51:30 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, msg, args) => {
+
+  let hEmbed = new Discord.MessageEmbed()
+  .setColor('5DF510')
+  .setTitle(`Pannel d'aide`)
+  .setDescription('Ici vous trouverez toutes les commandes disponibles !')
+  .addField('En dev..', 'En dev..')
+  .addField('En dev..', 'En dev..')
+  .addField('En dev..', 'En dev..')
+  .setFooter('Support serveur : ')
+  .setTimestamp();
+};
+
+module.exports.help = {
+  name: "help"
+};
