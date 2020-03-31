ee20bcb273c498b5dffd4d78db798748cca5abb4
Victorien01 <victorienboin95@gmail.com>
2020-03-31 16:23:41 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', async () => {
+  bot.user.setActivity("Je suis le nouveau !")
+});
+
+bot.login(process.env.BOT_TOKEN)
