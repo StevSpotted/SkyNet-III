f7a641c0531400105338097877dd4f3a72fdf186
Victorien01 <victorienboin95@gmail.com>
2020-03-31 17:51:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const config = require("./config.js");
+
 
 bot.on('ready', async () => {
   bot.user.setActivity("Je suis le nouveau !")
 });
 
-bot.login(process.env.BOT_TOKEN)
+bot.login(process.env.BOT_TOKEN);
