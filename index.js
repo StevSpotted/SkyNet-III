43152393c87641fed76b2f726712331bd6f1c0a4
Victorien01 <victorienboin95@gmail.com>
2020-03-31 20:11:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,21 +18,6 @@ bot.on('message', async (msg) => {
     let command = commands.getCommand(cmd);
     if(command) command.run(bot, msg, args);
 
-    if(cmd == 'help'){
-
-            let hEmbed = new Discord.MessageEmbed()
-            .setColor('5DF510')
-            .setTitle(`Pannel d'aide`)
-            .setDescription('Ici vous trouverez toutes les commandes disponibles !')
-            .addField('En dev..', 'En dev..')
-            .addField('En dev..', 'En dev..')
-            .addField('En dev..', 'En dev..')
-            .setFooter('Support serveur : ')
-            .setTimestamp();
-            msg.channel.send(hEmbed);
-        }
-
-
   }
 
 });
