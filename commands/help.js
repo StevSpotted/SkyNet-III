b9d2507f085e1d31952ded4e5cd951f947df0e40
Victorien01 <victorienboin95@gmail.com>
2020-03-31 21:31:27 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, msg, args) => {
 
+let user = message.author
+
   let hEmbed = new Discord.MessageEmbed()
   .setColor('5DF510')
+  .setThumbnail(user.avatarURL)
   .setTitle(`Pannel d'aide`)
   .setDescription('Ici vous trouverez toutes les commandes disponibles !')
   .addField(`*administration`, `Vous donneras l'accès au commandes administrateurs`)
   .addField(`*modération`, `Vous donneras l'accès au commandes modérateurs`)
   .addField(`En dev`, 'En dev..')
-  .setFooter('Support serveur = (https://discord.gg/t2tqR3b)')
+  .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
   .setTimestamp();
   msg.channel.send(hEmbed);
 }
