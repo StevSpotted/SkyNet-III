72dcfea4570bb4accc48329ac9b152dddaa95570
Victorien01 <victorienboin95@gmail.com>
2020-04-02 01:33:08 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,12 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
 
-  if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`Vous n'avez pas les droit`);
-  if(!args[0]) return message.channel.send(`Merci d'indiquer du texte`);
-  message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`${args[0]} messages supprimés !`).then(msg => msg.delete(5000))
-  });
-};
+
 
 module.exports.help = {
   name: 'clear'
