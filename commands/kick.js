e3401307a6dd0fc31eca0d1195879baeaa91617b
Victorien01 <victorienboin95@gmail.com>
2020-04-01 17:55:43 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
 
-    if(!msg.member.hasPermission("KICK_MEMBERS") return msg.channel.send("Vous n'avez pas le droit de kick."));
-    if(!msg.guild.me.hasPermission("KICK_MEMBERS") return msg.channel.send("Je n'ai pas les permissions"));
+    if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send("Vous n'avez pas le droit de kick.");
+    if(!msg.guild.me.hasPermission("KICK_MEMBERS")) return msg.channel.send("Je n'ai pas les permissions");
     if(args.length < 2) return msg.channel.send("Donnez moi une raison");
     const member = msg.mentions.members.first();
     let reason = '';
