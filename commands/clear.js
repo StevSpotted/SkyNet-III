68da2c50e4f6663d819098c8a5633eff431e0cc7
Victorien01 <victorienboin95@gmail.com>
2020-04-02 01:21:01 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ module.expotrs.run = async (bot, msg, args) => {
 
   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`Vous n'avez pas les droit`);
   if(!args[0]) return message.channel.send(`Merci d'indiquer du texte`);
-  message.channel.bulkDelete(args[0]).then(()) => {
+  message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`${args[0]} messages supprimés !`).then(msg => msg.delete(5000))
   });
 };
