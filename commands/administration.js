2a74a0bc75696d6beaf6bd91dcfa7c9752efe0fe
Victorien01 <victorienboin95@gmail.com>
2020-04-02 21:32:26 +0200

    Create administration.js

---
+++ commands/administration.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, msg, args) => {
+
+
+//let user;
+if(!msg.member.hasPermission('MANAGE_SERVER')) return msg.reply(`Petit malin tu n'a pas les permission`)
+
+  let aEmbed = new Discord.MessageEmbed()
+  .setColor('5DF510')
+  //.setThumbnail(bot.user.avatarURL)
+  .setTitle(`**Pannel d'administration'**`)
+  .setDescription(`Commande d'administration disponible`)
+  .addField(`*say`, `Permet de faire parler le bot`)
+  .addField(`*En cours`, `En cours`)
+  .addField(`En cours`, 'En cours')
+  //.setFooter(`Replying to ${message.author.username}`)
+  .setTimestamp();
+  msg.channel.send(aEmbed);
+
+
+
+}
+
+module.exports.help = {
+  name: "administration"
+}
