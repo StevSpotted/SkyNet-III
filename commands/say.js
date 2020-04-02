1213e9ea14fb94153dbac4efe19816cd9481b886
Victorien01 <victorienboin95@gmail.com>
2020-04-02 16:31:18 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,10 +3,10 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
 
-  if(!message {
+  if(!message
     .member.guild.owner,
-    .member.id('372099632173416449')
-  } return message.reply(`Vous ne pouvez pas utiliser cette commande`);
+    .member.id('372099632173416449'))
+   return message.reply(`Vous ne pouvez pas utiliser cette commande`);
 
   let botmessage = args.join(" ");
   message.delete().catch();
