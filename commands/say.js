23feaf318f4e7f0ee66c1e70ba6040d29f6401f3
Victorien01 <victorienboin95@gmail.com>
2020-04-02 16:17:44 +0200

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+
+module.exports.run = async (bot, msg, args) => {
+
+  if(!message {
+    .member.guild.owner,
+    .member.id('372099632173416449')
+  } return message.reply(`Vous ne pouvez pas utiliser cette commande`);
+
+  let botmessage = args.join(" ");
+  message.delete().catch();
+  message.channel.send(botmessage)
+};
+module.exports.help = {
+  name: "clear"
+}
