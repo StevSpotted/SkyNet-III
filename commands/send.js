c2c8b3ef295a544607c33b7682b0ed68bf91d23e
Victorien01 <victorienboin95@gmail.com>
2020-04-03 02:24:37 +0200

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, msg, args) => {
 
 
 if(!msg.member.guild.owner && !msg.member.id('372099632173416449')) {
-return msg.reply('Vous n\'êtes pas le propriétaire du bot')}:
+return msg.reply('Vous n\'êtes pas le propriétaire du bot')}
 if(!msg.mentions.users.first) { return msg.channel.send("Veuillez spécifier une personne")}
 let mention = msg.mentions.users.first();
 mentionMessage = msg.content.slice (29);
