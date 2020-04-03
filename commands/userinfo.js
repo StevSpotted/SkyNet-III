f11ae0f124fc3a704fae62104594257c549dc22c
Victorien01 <victorienboin95@gmail.com>
2020-04-03 02:09:15 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const moment = require('moment')
 
 module.exports.run = async (bot, msg, args) => {
+  if(!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.reply(`Tu n'as pas les droits mon petit !`)
   let userToCheck = msg.mentions.users.first();
   if(!userToCheck) userToCheck = msg.author;
 
@@ -16,6 +17,7 @@ module.exports.run = async (bot, msg, args) => {
   .addField(`Status :`, `${userToCheck.presence.status}`, true)
   .addField(`Joue à :`, `${userToCheck.presence.game ? userToCheck.presence.game.name : "Nothing"}`, true)
   .addField('Créé le :', `${moment.utc(userToCheck.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+  .addField(`A rejoint le serveur:`, `${moment.utc(userToCheck.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
 msg.channel.send(uEmbed)
 }
 
