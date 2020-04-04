5e988a5a7492dbdb28d2be5f5c0c2dd082b13428
Victorien01 <victorienboin95@gmail.com>
2020-04-04 18:38:20 +0200

    Update globalchat.js

--- commands/globalchat.js
+++ commands/globalchat.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
 
-  if(!message.member.hasPermission('ADMINISTRATOR')) {
-    return message.reply("Désolé tu n'as pas les permissions administrateur")
+  if(!msg.member.hasPermission('ADMINISTRATOR')) {
+    return msg.reply("Désolé tu n'as pas les permissions administrateur")
   }
-  if(!message.member.me.hasPermission('ADMINISTRATOR')) {
-    return message.reply("Désolé je n'ai pas les permissions administrateur")
+  if(!msg.member.me.hasPermission('ADMINISTRATOR')) {
+    return msg.reply("Désolé je n'ai pas les permissions administrateur")
   }
-  message.guild.createChannel("global-chat").then(channel => {
+  msg.guild.createChannel("global-chat").then(channel => {
     channel.setTopic('global-chat')
   });
 
