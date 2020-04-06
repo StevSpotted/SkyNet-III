210121be55d1a181709c4556bbcd7aa263c57d07
Victorien01 <victorienboin95@gmail.com>
2020-04-06 17:24:49 +0200

    Update administration.js

--- commands/administration.js
+++ commands/administration.js
@@ -7,8 +7,8 @@ if(!msg.member.hasPermission('MANAGE_SERVER')) return msg.reply(`Petit malin tu 
 
   let aEmbed = new Discord.MessageEmbed()
   .setColor('5DF510')
-  .setThumbnail(bot.user.avatarURL())
-  .setTitle(`**Pannel d'administration'**`)
+  .setThumbnail(user.avatarURL())
+  .setTitle(`**Pannel d'administration**`)
   .setDescription(`Commande d'administration disponible`)
   .addField(`*say`, `Permet de faire parler le bot`)
   .addField(`*En cours`, `En cours`)
