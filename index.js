b9bd59c6b391d27882fdc6c1e40ceb147abbc7ce
Victorien01 <victorienboin95@gmail.com>
2020-04-06 11:12:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on('message', async (msg) => {
                .setTimestamp()
 
 
-   let channelanno = bot.guilds.collection.find(channel => channel.name === 'global-chat').send(gEmbed)
+   let channelanno = bot.guilds.textChannels(channel => channel.name === 'global-chat').send(gEmbed)
 
  }
 
