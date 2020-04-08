9383cdfae00d80afeb33b53bf77b79ba5ea6a230
Victorien01 <victorienboin95@gmail.com>
2020-04-08 11:51:54 +0200

    Create énigme.js

---
+++ commands/énigme.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, msg, args) => {
+msg.delete();
+ let eEmbed = new Discord.MessageEmbed()
+
+ .setColor('0x1A10F5')
+ .setDescription(`Énigme à résoudre`)
+ .setTitle(`Énigme résolu !`)
+ .addField(`Le mot que m'avait dit mon créateur a été trouvé par <@598197511882276894>`)
+ .addField(`Le mot été **Update** `, `Update de qui , Update de quoi ?`)
+ .addField(`C'est ce que vous aller devoir tenter de savoir !`, `Comme le nom l'indique c'est une énigme, creusez vous la tête !`)
+ .addField(`Cette énigme comportera plusieurs problème`)
+ .addField( `Et d'ailleurs le premier problème c'est **주문**`, `Òui moi non plus j'ai pas trop compris mais il ma dit que ça avait du sens`)
+ .setFooter(`Je vous recontacterai si j'ai du nouveau`)
+}
+module.exports.help = {
+  name: "énigme"
+}
