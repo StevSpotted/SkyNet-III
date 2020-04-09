a81efd2352b60c7b71f0fc89c5a78d7147d9ca96
Victorien01 <victorienboin95@gmail.com>
2020-04-09 17:17:02 +0200

    Update administration.js

--- commands/administration.js
+++ commands/administration.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const prefix = "*"
 module.exports.run = async (bot, msg, args) => {
 
 
@@ -10,9 +11,9 @@ if(!msg.member.hasPermission('MANAGE_SERVER')) return msg.reply(`Petit malin tu 
   .setThumbnail(msg.author.avatarURL())
   .setTitle(`**Pannel d'administration**`)
   .setDescription(`Commande d'administration disponible`)
-  .addField(`*say`, `Permet de faire parler le bot`)
-  .addField(`*En cours`, `En cours`)
-  .addField(`En cours`, 'En cours')
+  .addField(`**${prefix}say**`, `*Permet de faire parler le bot*`)
+  .addField(`**${prefix}setupGC`, `*Permet de créer un chat textuels inter-connecté avec les autres*`)
+  .addField(`**${prefix}FuturUpdate ?`, '*Potentiellement une deuxième énigme*')
   .setFooter(`Replying to ${msg.author.username}`)
   .setTimestamp();
   msg.channel.send(aEmbed);
