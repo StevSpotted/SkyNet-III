20ea810299775dcf22b1bb09fbafc40aa7a22980
Victorien01 <victorienboin95@gmail.com>
2020-04-09 17:17:14 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,10 +11,13 @@ let user;
   .setThumbnail(msg.author.avatarURL())
   .setTitle(`Pannel d'aide`)
   .setDescription('Ici vous trouverez toutes les commandes disponibles !')
-  .addField(`${prefix}administration`, `Vous donneras l'accès au commandes administrateurs`)
-  .addField(`*modération`, `Vous donneras l'accès au commandes modérateurs`)
-  .addField(`**Ici se situera la boutique du serveur**`, `[Allez à la boutique]En cours`)
-  .addField(`Support server`, '[Rejoindre ce magnifique serveur](https://discordapp.com/invite/t2tqR3b)')
+  .addField(`**${prefix}administration**`, `*Vous donneras l'accès au commandes administrateurs*`)
+  .addField(`**${prefix}modération**`, `*Vous donneras l'accès au commandes modérateurs*`)
+  .addField(`**${prefix}invitation**`, `*Vous donneras le lien pour m'inviter dans vos serveurs*`)
+  .addField(`**${prefix}commandes**`, `*Vous donnes un accès au commmandes du bot*`)
+  .addField(`**${prefix}FuturUpdate ?**`, `*Potentiellement une deuxième égnime*`)
+  .addField(`**Ici se situera la boutique du serveur**`, `*[Allez à la boutique] En cours...*`)
+  .addField(`**Support server**`, '[Rejoindre ce magnifique serveur](https://discordapp.com/invite/t2tqR3b)')
   .setFooter(`Replying to ${msg.author.username}`)
   .setTimestamp();
   msg.channel.send(hEmbed);
