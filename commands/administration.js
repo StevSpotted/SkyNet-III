2bbc083d8e9f1a466522097dc783b20f3295c5f6
Victorien01 <victorienboin95@gmail.com>
2020-04-09 17:20:12 +0200

    Update administration.js

--- commands/administration.js
+++ commands/administration.js
@@ -12,8 +12,8 @@ if(!msg.member.hasPermission('MANAGE_SERVER')) return msg.reply(`Petit malin tu 
   .setTitle(`**Pannel d'administration**`)
   .setDescription(`Commande d'administration disponible`)
   .addField(`**${prefix}say**`, `*Permet de faire parler le bot*`)
-  .addField(`**${prefix}setupGC`, `*Permet de créer un chat textuels inter-connecté avec les autres*`)
-  .addField(`**${prefix}FuturUpdate ?`, '*Potentiellement une deuxième énigme*')
+  .addField(`**${prefix}setupGC**`, `*Permet de créer un chat textuels inter-connecté avec les autres*`)
+  .addField(`**${prefix}FuturUpdate** ?`, '*Potentiellement une deuxième énigme*')
   .setFooter(`Replying to ${msg.author.username}`)
   .setTimestamp();
   msg.channel.send(aEmbed);
