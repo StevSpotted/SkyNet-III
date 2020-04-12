69d2e72c7ab85279df665187c62ec728394172ac
Victorien01 <victorienboin95@gmail.com>
2020-04-12 17:52:11 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, msg, args) => {
     if(!member) return msg.channel.send("Le membre est invalide.");
     member.kick();
 
-    let hEmbed = new Discord.MessageEmbed()
+    let hEmbed = new Discord.RichEmbed()
     .setColor("#00e68a")
     .setTitle("Rapport de kick")
     .setDescription(`Un kick a été effectué par ${msg.author}`)
